6241fe89542d5560769af948a13b2d0f1e713907
AnthonySabbagh <asabb087@uottawa.ca>
2019-03-25 12:03:59 -0400

    Added search UI to hotes component.

--- client/src/components/Hotels.js
+++ client/src/components/Hotels.js
@@ -20,6 +20,169 @@ export class Hotels extends Component {
       <Fragment>
         <Navbar />
         <h1 className="display-4 my-3">Hotels</h1>
+        <div class="container">
+          <form>
+            <div class="form-row">
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="startInput">
+                      Start Date:
+                    </span>
+                  </div>
+                  <input type="date" class="form-control" id="startInput" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="endInput">
+                      End Date:
+                    </span>
+                  </div>
+                  <input type="date" class="form-control" id="endInput" />
+                </div>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="cityInput"
+                  placeholder="City"
+                />
+              </div>
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="stateInput"
+                  placeholder="State/Province"
+                />
+              </div>
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="chainInput"
+                  placeholder="Preferred hotel chain"
+                />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="minInput">
+                      Min Price: $
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="minInput"
+                    placeholder="Any"
+                    min="0"
+                    step="10"
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="maxInput">
+                      Max Price: $
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="maxInput"
+                    placeholder="Any"
+                    min="0"
+                    step="10"
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="capacityInput">
+                      Capacity:
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="capacityInput"
+                    placeholder="Any"
+                    min="1"
+                    max="12"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="minRoomsInput">
+                      Min # of Rooms in Hotel:
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="minRoomsInput"
+                    placeholder="Any"
+                    min="0"
+                    step="25"
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="maxRoomsInput">
+                      Max # of Rooms in Hotel:
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="maxRoomsInput"
+                    placeholder="Any"
+                    min="0"
+                    step="25"
+                    max="10000"
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="ratingInput">
+                      Star Rating:
+                    </span>
+                  </div>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="ratingInput"
+                    placeholder="Any"
+                    min="1"
+                    max="5"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
         <Query query={HOTELS_QUERY}>
           {({ loading, error, data }) => {
             if (loading) {
